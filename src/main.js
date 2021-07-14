ae85cc0a7e6bbc303413209b028e38fd52073f56
Anjaliba Gohil <codexprt2@gmail.com>
2021-07-14 14:13:24 +0530

    Game view Done

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Game from "./Game.js";
+import GameView from "./GameView.js";
+
+let game = new Game();
+let gameView = new GameView();
+
+let tiles = document.querySelectorAll(".board-tile");
+
+tiles.forEach((tile) => {
+	tile.addEventListener("click", () => {
+		console.log("Tile Clicked!!!");
+		onTileClick(tile.dataset.index);
+	});
+});
+
+function onTileClick(i) {
+	// do something
+	game.makeMove(i);
+
+	// update board
+	gameView.updateBoard(game);
+	// change turn
+	game.nextTurn();
+}
